98cfa698691378e5d12df093fb0b4f31a0f90a08
Nikolay Ilyin <irishwithaxe@gmail.com>
2024-04-29 15:55:53 +0300

    bugfixes

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 import datetime
 import pathlib
 import time
+import traceback
 
-city = "sfbay"
+city = "sf-bay"
 sim_years = [2077, 2078, 2079]
 finish_step_delay_seconds = 1
 
@@ -24,9 +25,12 @@ def create_files(parent_folder, list_of_files):
     folder = pathlib.Path(parent_folder)
     folder.mkdir(parents=True, exist_ok=True)
     log(f"Created folder '{folder}'")
-    for file in list_of_files:
-        with file in open((folder / file).resolve(), "a+"):
-            file.write(f"this MOCK record written at {str(datetime.datetime.now())}")
+    for file_name in list_of_files:
+        full_file_path = (folder / file_name).resolve()
+        file_mode = "a+" if full_file_path.exists() else "w+"
+        with open(full_file_path, file_mode) as file:
+            a_record = f"this MOCK record written at {str(datetime.datetime.now())}\n"
+            file.write(a_record)
 
 
 def do_activitysim_step(year=2077):
@@ -49,12 +53,12 @@ def do_beam_step(year=2077):
 
 
 def do_urbansim_step(year=2077):
-    create_files(f"pilates/urbansim/output/year-{year}", "urbansim-results.txt")
+    create_files(f"pilates/urbansim/output/year-{year}", ["urbansim-results.txt"])
     finish_step(stage="Urbansim", year=year)
 
 
 def do_postprocessing_step(years):
-    create_files(f"pilates/postprocessing/output", ["a_postprocess_result.txt"])
+    create_files(f"pilates/postprocessing/output", ["a_post_process_result.txt"])
     for year in years:
         create_files(f"pilates/postprocessing/MEP/{year}",
                      ["plans.txt", "persons.txt", "jobs.txt", "households.txt"])
@@ -63,12 +67,16 @@ def do_postprocessing_step(years):
 
 if __name__ == "__main__":
     try:
+        log(f"Going to run pilates for city {city}, years {sim_years}")
         for sim_year in sim_years:
+            log(f"Processing year {sim_year}")
             do_activitysim_step(sim_year)
             do_urbansim_step(sim_year)
             do_beam_step(sim_year)
 
         do_postprocessing_step(sim_years)
+        log("Finished")
 
     except Exception as err:
+        traceback.print_exc()
         log(f"Exception during execution: {str(err)}")
