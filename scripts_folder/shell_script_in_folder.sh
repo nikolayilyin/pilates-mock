fa885eadfe07b1037fba284b4c78d484ae2b898e
Nikolay Ilyin <irishwithaxe@gmail.com>
2024-05-01 17:20:13 +0300

    + shell script mocks

---
+++ scripts_folder/shell_script_in_folder.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Shell script in folder executing at $(date "+%Y-%m-%d-%H:%M:%S")" > shell_script_in_folder.log
+sleep 3s
+echo "Shell script in folder executed at $(date "+%Y-%m-%d-%H:%M:%S")" > shell_script_in_folder.log
---
+++ shell_script_in_root.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Shell script in root executing at $(date "+%Y-%m-%d-%H:%M:%S")" > shell_script_in_root.log
+sleep 3s
+echo "Shell script in root executed at $(date "+%Y-%m-%d-%H:%M:%S")" > shell_script_in_root.log
