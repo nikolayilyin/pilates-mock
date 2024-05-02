76aedb7e097b1d9ccd6c54b76d5df73324f12341
Nikolay Ilyin <irishwithaxe@gmail.com>
2024-05-02 14:06:50 +0300

    fixes for shell scripts

--- scripts_folder/shell_script_in_folder.sh
+++ scripts_folder/shell_script_in_folder.sh
@@ -1,7 +1,12 @@
-#!/bin/bash -x
+#!/bin/bash
 
 full_out_path=$(realpath log_from_shell_script_in_folder.log)
+log() {
+  echo "$1" | tee -a "$full_out_path"
+}
 
-echo "Shell script in folder executing at $(date "+%Y-%m-%d-%H:%M:%S")" >> "$full_out_path"
-sleep 3s
-echo "Shell script in folder executed at $(date "+%Y-%m-%d-%H:%M:%S")" >> "$full_out_path"
+timeout="3s"
+
+log "Shell script in folder executing at $(date "+%Y-%m-%d-%H:%M:%S")"
+log "Going to sleep for $timeout" && sleep 3s
+log "Shell script in folder executed at $(date "+%Y-%m-%d-%H:%M:%S")"
--- shell_script_in_root.sh
+++ shell_script_in_root.sh
@@ -1,7 +1,12 @@
-#!/bin/bash -x
+#!/bin/bash
 
 full_out_path=$(realpath log_from_shell_script_in_root.log)
+log() {
+  echo "$1" | tee -a "$full_out_path"
+}
 
-echo "Shell script in root executing at $(date "+%Y-%m-%d-%H:%M:%S")" >> "$full_out_path"
-sleep 3s
-echo "Shell script in root executed at $(date "+%Y-%m-%d-%H:%M:%S")" >> "$full_out_path"
+timeout="3s"
+
+log "Shell script in root executing at $(date "+%Y-%m-%d-%H:%M:%S")"
+log "Going to sleep for $timeout" && sleep 3s
+log "Shell script in root executed at $(date "+%Y-%m-%d-%H:%M:%S")"
